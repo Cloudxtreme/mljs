07f82ce78e4eef7f8c9feaba4f621ed9e8cde939
libra guest <ov.whited@yahoo.com>
2012-10-16 23:21:10 -0400

    Implement video

--- ml_api.js
+++ ml_api.js
@@ -11,14 +11,7 @@ var add_video = function(thumbs_arr, vid_url) {
  */
 var create_video_tile = function() {
     var tile = $(document.createElement('div'));
-    var video = document.createElement('video');
-    var jqvideo = $(video);
-    jqvideo.css({'width':'100%','height':'100%'});
-    jqvideo.hide();
-    video.src = "http://www.mediacollege.com/video/format/mpeg4/videofilename.mp4";
-    video.controls = true;
-    tile.append(jqvideo);
-    tile.css_props = {'backgroundColor':'#FF0000','border-radius':'15px','width':'100','height':'100'};
+    tile.css_props = {'position':'static','margin':'5px','float':'left','backgroundColor':'#FF0000','border-radius':'15px','width':'100','height':'100'};
     tile.css(tile.css_props);
     tile.view_mode = 0; // 0 = inactive, 1 = thumbnail, 2 = playing
     tile.click(function() {
@@ -29,20 +22,36 @@ var create_video_tile = function() {
         }
         else if(tile.view_mode === 1) {
             // User has clicked the tile again, time to play the video
-            jqvideo.show();
-            tile.animate({'width':'500px', 'height':'500px'}, 250, function() {
-                video.play();
-            });
+            show_video_overlay(tile);
+            
             tile.view_mode++;
-        }
-        else if(tile.view_mode === 2) {
-            // Stop playing and go back to thumbnail mode
-            tile.animate(tile.css_props, 250, function() {
-                video.load();
-                jqvideo.hide();
-            });
-            tile.view_mode = 0;
         }
     });
     return tile;
+};
+
+var show_video_overlay = function(tile) {
+    tile.overlay = $(document.createElement('div'));
+            tile.overlay.css({'clear':'both','position':'fixed','backgroundColor':'#FF0000','border-radius':'15px'});
+            tile.overlay.click(function() {
+                // Stop playing and go back to thumbnail mode
+                tile.overlay.animate(tile.css_props, 250, function() {
+                    tile.animate(tile.css_props, 250);
+                });
+                tile.overlay.remove();
+                tile.overlay = undefined;
+                tile.view_mode = 0;
+            });
+            var video = document.createElement('video');
+            var jqvideo = $(video);
+            jqvideo.css({'width':'100%','height':'100%'});
+            video.src = "http://www.mediacollege.com/video/format/mpeg4/videofilename.mp4";
+            video.controls = true;
+            tile.overlay.append(video);
+            jqvideo.show();
+            tile.overlay.show();
+            $(document.body).append(tile.overlay);
+            tile.overlay.animate({'left':'5px','top':'5px','right':'5px','bottom':'5px'}, 250, function() {
+                video.play();
+            });
 };
