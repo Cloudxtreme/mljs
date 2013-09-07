2d6648662cd318eeb0059850d6e097371ecb1728
Whired <ov.whited@gmail.com>
2013-09-07 16:35:34 -0400

    Updates UI to accommodate ML changes

--- ml_ui.js
+++ ml_ui.js
@@ -13,41 +13,45 @@ var author_mode;
 
 // Creates a tile for the specified video
 var create_video_tile = function(video) {
-	
-    // The target size of the file
-    var tile_size = {width:'121px',height:'100px',margin:'5px'};
-    
     // Create the elements required for a tile
-    var tile = $(document.createElement('div'));
-    tile.border_overlay = $(document.createElement('div'));
-    var thumb_strip_container = $(document.createElement('div'));
-    tile.thumb_strip = $(document.createElement('img'));
-	var new_ttl = $(document.createElement('div'));
-	var new_nfo = $(document.createElement('div'));
+    var tile = $('#tile').clone(true);
+    tile.thumb_strip = tile.find('#thumbstrip');
 
-    // Set styles
-    var b_s = {'overflow':'hidden','width':tile_size.width};
-    tile.border_overlay.css(b_s);
-    tile.border_overlay.border_css = {'background-image':'url(images/ovr.png)','background-position-x':'0','border-radius':'15px','position':'absolute','height':'100%','width':'100%','pointer-events':'none'};
-    tile.border_overlay.css(tile.border_overlay.border_css);
-    thumb_strip_container.css(b_s).css({'position':'relative','float':'left','border-top-left-radius':'15px','border-top-right-radius':'15px','height':'130px'});
-    tile.css(b_s).css({'border-radius':'15px','background':'#454545','position':'relative','margin':tile_size.margin,'float':'left','cursor':'pointer'});
-    tile.thumb_strip.css({'margin-top':'15px','width':'607px','height':'100'});
-    var c_s = {'position':'absolute','left':'15px','right':'15px','height':'13px','overflow':'hidden','white-space':'nowrap'};
-    new_ttl.css(c_s).css({'top':'2px'});
-    new_nfo.css(c_s).css({'bottom':'0px','color':'#FFF'});
+    // Configure tile
+    tile.thumb_strip.css({'background-image':'url('+video.thumbnail_url+')'});
+    tile.find('#title').prop('href',video.full_url).text(video.title);
+    tile.find('#length').text(video.length);
+    tile.find('#author').text(video.author).click(function(evt) {
+            evt.stopPropagation();
+            if(!author_mode) {
+                load_user_videos(video.author,function(cbv) {
+                    if(cbv.length > 0) {
+                        author_mode = true;
+                        var d = $('#drawer');
+                        $(cbv).each(function() {
+                            $(d).append(create_video_tile(this));
+                        });
+                        // Open the drawer
+                        $(d).fadeIn(700, function() {
+                            // Hide normal content
+                            $('#content').hide();
+                        });   
+                    }
+                });
+            }
+        });
+    tile.css({'display':'block'});
 
     // Set other properties
 	tile.video = video;
 	tile.thumb_strip.left_offset = 0;
-    tile.thumb_strip.get(0).src = video.thumbnail_url;
     tile.view_mode = 0; // 0 = inactive, 1 = thumbnail, 2 = playing
     tile.click(function(evt) {
         evt.stopPropagation();
         if(tile.view_mode === 0) {
             // First click, show thumbnail/slideshow
 			tile.view_mode++;
-			tile.border_overlay.css({'background-position-x':'-121'});
+			tile.css({'background-color':'#6495ED'});
 			cycle_thumbs(tile);
         }
         else if(tile.view_mode === 1) {
@@ -58,35 +62,11 @@ var create_video_tile = function(video) {
         }
     });
 
-	// Build the tile
-	thumb_strip_container.append(new_ttl.append($(document.createElement('a')).prop('href',video.full_url).append(video.title)))
-		.append(tile.thumb_strip)
-		.append(new_nfo.append(video.length).append(' | ').append(video.author).click(function(evt) {
-            evt.stopPropagation();
-            if(!author_mode) {
-                load_user_videos(video.author,function(cbv) {
-                    if(cbv.length > 0) {
-                        author_mode = true;
-                        $(cbv).each(function() {
-                            $(drawer).append(create_video_tile(this));
-                        });
-                        // Open the drawer
-                        $(drawer).fadeIn(700, function() {
-                            // Hide normal content
-                            $(content).hide();
-                        });   
-                    }
-                });
-            }
-        }));
-    tile.append(thumb_strip_container);
-    tile.append(tile.border_overlay);
-	
 	// Add taboo notification if applicable
     if(video.taboo) {
 		tile.icon_overlay = $(document.createElement('div'));
 		tile.icon_overlay.prop('title','tagged as '+video.taboo).css({'background':'url(images/'+video.taboo+'.png) no-repeat center center','width':'24px','height':'24px','position':'absolute','right':'3px','top':'17px'})
-		.appendTo(tile.border_overlay);
+		.appendTo(tile);
 	}
 	
     return tile.get(0);
@@ -97,9 +77,8 @@ var cycle_thumbs = function(tile) {
 	clear_preview(current_preview_tile);
 	// Used for cycling immediatly
 	var cycle = function() {
-		if(tile.thumb_strip.left_offset < tile.thumb_strip.width()-242) {
-			
-            tile.thumb_strip.left_offset += 121;
+		if(tile.thumb_strip.left_offset < tile.thumb_strip.width()-400) {
+            tile.thumb_strip.left_offset += 200;
             tile.thumb_strip.get(0).style.marginLeft = -tile.thumb_strip.left_offset+'px';
         }
         else {
@@ -108,7 +87,7 @@ var cycle_thumbs = function(tile) {
         }
 	};
 	cycle();
-    tile.interval = setInterval(cycle, 1000);
+    tile.interval = setInterval(cycle, 800);
 	current_preview_tile = tile;
 };
 
@@ -117,7 +96,7 @@ var clear_preview = function(tile) {
 	if(tile) {
 		clearInterval(tile.interval);
 		tile.view_mode = 0;
-		tile.border_overlay.css(tile.border_overlay.border_css, 1000);
+        tile.css({'background-color':'#4169E1'});
 	}
 };
 
@@ -165,5 +144,6 @@ $(document).ready(function() {
 			$(content).append(create_video_tile(this));
 		});
 	});
-	load_live_videos();
+	
+    load_live_videos();
 });
